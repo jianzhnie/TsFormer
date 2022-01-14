43caf768953f78dbe89d524b56637804fecacb17
jianzheng.nie <jianzhnie@126.com>
2022-01-14 18:40:23 +0800

    add scripts

---
+++ scripts/ETTm2_script/GRU.sh
@@ -0,0 +1,31 @@
+export CUDA_VISIBLE_DEVICES=0
+###
+ # @Author: jianzhnie
+ # @Date: 2022-01-13 15:20:03
+ # @LastEditTime: 2022-01-14 18:30:30
+ # @LastEditors: jianzhnie
+ # @Description:
+ #
+###
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/ETT-small/ \
+  --data_path ETTm2.csv \
+  --model_id ETTm2 \
+  --model gru \
+  --data RNNData \
+  --features MS \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --freq t \
+  --input_size 7 \
+  --hidden_size 256 \
+  --num_layers 1 \
+  --output_size 96 \
+  --train_epochs 20 \
+  --batch_size 32 \
+  --learning_rate 0.0001 \
+  --gpu 0 \
+  --des 'Exp' \
+  --itr 1
---
+++ scripts/house_power/RNN_ETTh1.sh
@@ -0,0 +1,32 @@
+export CUDA_VISIBLE_DEVICES=0
+###
+ # @Author: jianzhnie
+ # @Date: 2022-01-13 15:20:03
+ # @LastEditTime: 2022-01-14 18:15:21
+ # @LastEditors: jianzhnie
+ # @Description:
+ #
+###
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/house_power/ \
+  --data_path UCI_household_power_consumption_synth_hour.csv \
+  --model_id UCI_household_power \
+  --model rnn \
+  --data RNNData \
+  --target Global_active_power \
+  --features S \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --input_size 1 \
+  --hidden_size 256 \
+  --num_layers 1 \
+  --output_size 96 \
+  --train_epochs 20 \
+  --batch_size 256 \
+  --learning_rate 0.001 \
+  --gpu 0 \
+  --des 'Exp' \
+  --itr 1
